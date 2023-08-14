829af84cd0120628e6c9198dbdc746f951ffdbb0
Raman Fedaseyeu <raman.fedaseyeu@gmail.com>
2023-08-14 19:24:28 +0200

    Delete file loading via mmap
    because mmap doesn't seem to provide any benefits for the use-cases of this project

--- reptr/src/common.rs
+++ reptr/src/common.rs
@@ -2,11 +2,9 @@ use std::{fs, io, path::Path};
 
 mod error_context;
 mod read_dir_ext;
-mod source;
 
 pub use error_context::ErrorContext;
 pub use read_dir_ext::ReadDirExt;
-pub use source::Source;
 
 pub fn is_dart_pkg(path: &Path) -> io::Result<bool> {
     let has_manifest = {
@@ -27,17 +25,6 @@ pub fn is_dart_pkg(path: &Path) -> io::Result<bool> {
     Ok(has_manifest && (has_lib(path)? || has_bin(path)?))
 }
 
-pub fn try_load(path: &Path) -> io::Result<Source> {
-    fs::read_to_string(path)
-        .context_lazy(|| format!("Cannot read file at path {path:?}"))
-        .map(|s| s.into())
-}
-
-pub fn _try_mmap(path: &Path) -> io::Result<Source> {
-    let f = fs::File::open(path).context_lazy(|| format!("Cannot open file at path {path:?}"))?;
-    let mmap = unsafe { memmap2::Mmap::map(&f) }
-        .context_lazy(|| format!("Cannot memory-map file at path {path:?}"))?;
-
-    mmap.try_into()
-        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
+pub fn try_load(path: &Path) -> io::Result<String> {
+    fs::read_to_string(path).context_lazy(|| format!("Cannot read file at path {path:?}"))
 }
